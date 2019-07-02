1b3e08b20b450fe121c59a338ca6b6e47f3e0eea
Waxol <43796355+Waxol@users.noreply.github.com>
2019-07-02 13:54:08 +0200

    Add files via upload

---
+++ bot.js.js
@@ -0,0 +1,20 @@
+const botSettings = require('./botsettings.json');
+const Discord = require('discord.js');
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.login(botSettings.token); 
+
+bot.on('ready', async () => {
+	console.log(`Bot en ligne ! ${bot.user.username}`);
+});
+
+bot.on('message', async message => {
+	if(message.author.bot) return;
+    if(message.channel.type === 'dm') {
+        let messageContent = message.content
+        console.log(messageContent)
+
+        let anonChannel = bot.channels.find('id', '486116492509970433')
+        anonChannel.send(messageContent)
+    }
+});
